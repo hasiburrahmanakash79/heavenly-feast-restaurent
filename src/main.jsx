ca03d42963352d76992eb8d47429a28bfe8c636e
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-01 10:53:52 +0600

    make a hook for add to cart hook

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,20 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 import AuthProvider from "./Provider/AuthProvider";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="max-w-screen-2xl mx-auto">
     <React.StrictMode>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </AuthProvider>
     </React.StrictMode>
   </div>
