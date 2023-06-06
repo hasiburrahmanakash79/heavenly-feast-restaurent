0f71f4ead7335e15b7c2664135950ab9c33a27f4
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-06-06 14:36:46 +0600

    delete  items

--- src/Hook/useMenu.jsx
+++ src/Hook/useMenu.jsx
@@ -1,15 +1,24 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+// import { useEffect, useState } from "react";
 
 const useMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:5000/menu")
-      .then((res) => res.json())
-      .then((data) => {
-        setMenu(data);
-      });
-  }, []);
-  return [menu];
+  // const [menu, setMenu] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/menu")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setMenu(data);
+  //     });
+  // }, []);
+
+  const {data: menu = [], isLoading: loading, refetch} = useQuery({
+    queryKey: ['menu'],
+    queryFn: async() => {
+        const res = await fetch('http://localhost:5000/menu');
+        return res.json();
+    }
+})
+  return [menu, loading, refetch];
 };
 
 export default useMenu;
--- src/Pages/DashBoard/ManageItems/ManageItems.jsx
+++ src/Pages/DashBoard/ManageItems/ManageItems.jsx
@@ -2,29 +2,32 @@ import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import useMenu from "../../../Hook/useMenu";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import Swal from "sweetalert2";
+import useAxiosSecure from "../../../Hook/useAxiosSecure";
 
 const ManageItems = () => {
-  const [menu] = useMenu();
-
+  const [menu, , refetch] = useMenu();
+  const [axiosSecure] = useAxiosSecure();
   const handleDelete = (item) => {
     Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-          )
-        }
-      })
-  }
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/menu/${item._id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount) {
+            refetch();
+            Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          }
+        });
+      }
+    });
+  };
   return (
     <div className="p-5">
       <SectionTitle
@@ -52,7 +55,10 @@ const ManageItems = () => {
                 <td>
                   <div className="avatar">
                     <div className="w-16 rounded">
-                      <img src={item.image} alt="Tailwind-CSS-Avatar-component" />
+                      <img
+                        src={item.image}
+                        alt="Tailwind-CSS-Avatar-component"
+                      />
                     </div>
                   </div>
                 </td>
@@ -64,7 +70,10 @@ const ManageItems = () => {
                   </button>
                 </td>
                 <td>
-                  <button onClick={() => handleDelete(item)} className="text-white text-xl bg-red-700 p-3 rounded">
+                  <button
+                    onClick={() => handleDelete(item)}
+                    className="text-white text-xl bg-red-700 p-3 rounded"
+                  >
                     <FaTrashAlt></FaTrashAlt>
                   </button>
                 </td>
