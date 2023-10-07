bfab343909f4675a1d3368e0a79a5dfab3de86a9
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-07 20:39:07 +0600

    make contact us form

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,30 @@
 
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className="bg-gray-100 p-8">
+          <div className="max-w-2xl mx-auto">
+            <h1 className="text-3xl font-semibold mb-4">Contact Us</h1>
+            <p className="text-gray-700 mb-4">
+              Feel free to get in touch with us for any inquiries or bookings. We well be happy to assist you!
+            </p>
+            <form className="grid grid-cols-1 gap-6">
+              <div className="form-group">
+                <label htmlFor="name" className="text-gray-800 font-semibold">Name</label>
+                <input type="text" id="name" name="name" className="input-field" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" className="text-gray-800 font-semibold">Email</label>
+                <input type="email" id="email" name="email" className="input-field" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message" className="text-gray-800 font-semibold">Message</label>
+                <textarea id="message" name="message" rows="4" className="input-field" />
+              </div>
+              <button type="submit" className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition duration-300">Submit</button>
+            </form>
+          </div>
         </div>
-    );
+      );
 };
 
 export default Contact;
