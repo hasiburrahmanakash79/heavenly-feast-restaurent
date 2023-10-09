beb1290ffef196156cdf639350f9c50fd08a2c00
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-09 12:43:16 +0600

    make review porm

--- src/Pages/DashBoard/UserDashboard/AddReview/AddReview.jsx
+++ src/Pages/DashBoard/UserDashboard/AddReview/AddReview.jsx
@@ -21,67 +21,47 @@ const AddReview = () => {
       >
         <div>
           <label className="label">
-            <span className="label-text">Recipe name</span>
+            <span className="label-text">Which recipe you liked most?</span>
           </label>
           <input
             type="text"
-            {...register("name", { required: true })}
+            {...register("recipe", { required: true })}
             placeholder="Type here"
             className="input w-full"
           />
         </div>
-        <div className="grid grid-cols-2 gap-5">
-          <div>
-            <label className="label">
-              <span className="label-text">Category</span>
-            </label>
-            <select
-              defaultValue="Pick One"
-              {...register("category", { required: true })}
-              className="select w-full"
-            >
-              <option disabled>Pick One</option>
-              <option>pizza</option>
-              <option>soup</option>
-              <option>salad</option>
-              <option>drink</option>
-              <option>dessert</option>
-            </select>
-          </div>
-          <div>
-            <label className="label">
-              <span className="label-text">Price</span>
-            </label>
-            <input
-              {...register("price", { required: true })}
-              type="text"
-              placeholder="Type here"
-              className="input w-full"
-            />
-          </div>
+
+        <div>
+          <label className="label">
+            <span className="label-text">
+              Do you have any suggestion for us?
+            </span>
+          </label>
+          <input
+            {...register("suggestion", { required: true })}
+            type="text"
+            placeholder="Type here"
+            className="input w-full"
+          />
         </div>
         <div>
           <label className="label">
-            <span className="label-text">Recipe Details</span>
+            <span className="label-text">
+              Kindly express your care in a short way.
+            </span>
           </label>
           <textarea
             {...register("recipe", { required: true })}
             className="rounded-lg w-full p-4"
+            placeholder=" Recipe Details"
             rows="5"
           ></textarea>
         </div>
         <div>
           <input
-            type="file"
-            {...register("image", { required: true })}
-            className="file-input w-full max-w-xs my-3"
-          />
-        </div>
-        <div>
-          <input
             type="submit"
             value="Add Item"
-            className="btn bg-[#D1A054] border-none w-full "
+            className="btn bg-[#D1A054] border-none w-full mt-6"
           />
         </div>
       </form>
