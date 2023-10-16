d6dcbc4f1c634bf6cf45ec5a36485a201494aae6
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-17 00:24:09 +0600

    set user image in user homr

--- src/Pages/DashBoard/UserDashboard/UserHome/UserHome.jsx
+++ src/Pages/DashBoard/UserDashboard/UserHome/UserHome.jsx
@@ -3,7 +3,12 @@ const UserHome = () => {
     <div className="p-10">
       <h1 className="text-3xl font-semibold">Hi, Welcome Back! </h1>
       <div className="grid grid-cols-2 h-40">
-        <div className="bg-violet-300">
+        <div className="bg-violet-300 grid-flow-row justify-center items-center">
+          <div className="avatar">
+            <div className="w-40 rounded-full">
+              <img src="https://wac-cdn.atlassian.com/dam/jcr:ba03a215-2f45-40f5-8540-b2015223c918/Max-R_Headshot%20(1).jpg?cdnVersion=1261" />
+            </div>
+          </div>
           <h1>user name </h1>
         </div>
         <div className="bg-yellow-200">
