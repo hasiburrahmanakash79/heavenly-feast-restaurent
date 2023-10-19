9f9d0a5b2137238ec085f309c515d3879481edae
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-20 00:38:45 +0600

    make payment intent

--- src/Pages/DashBoard/UserDashboard/Payment/Payment.jsx
+++ src/Pages/DashBoard/UserDashboard/Payment/Payment.jsx
@@ -3,24 +3,40 @@ import UseTitle from "../../../../Hook/UseTitle";
 import useCart from "../../../../Hook/useCart";
 import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
 import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import { AuthContext } from "../../../../Provider/AuthProvider";
 
 const Payment = () => {
   UseTitle("Payment");
+  const { user } = useContext(AuthContext);
   const [cart] = useCart();
   const { id } = useParams();
-  console.log(id);
+  const items = cart.find((menu) => menu._id == id);
+  console.log(items);
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
   };
   return (
-    <div>
+    <div className="p-5">
       <SectionTitle subTitle={"Pay now"} title={"Payment"}></SectionTitle>
-      <div className="p-5">
+      <div className="md:grid grid-cols-2 bg-base-200 p-5 rounded">
+        <div className="flex justify-center items-center gap-5">
+          <div className="avatar">
+            <div className="w-full rounded">
+              <img src={items?.image} />
+            </div>
+          </div>
+          <div className="space-y-3">
+            <h1 className="text-3xl">{items?.name}</h1>
+            <h1 className="text-xl">Price: ${items?.price}</h1>
+            <h1>Description: {items?.recipe}</h1>
+          </div>
+        </div>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="bg-base-200 p-10 rounded-lg md:w-1/2 mx-auto"
+          className="p-10 rounded-lg mx-auto"
         >
           <div className="grid grid-cols-3 gap-10">
             <div>
@@ -31,17 +47,7 @@ const Payment = () => {
                 type="text"
                 {...register("name", { required: true })}
                 placeholder="Type here"
-                className="input w-full"
-              />
-            </div>
-            <div>
-              <label className="label">
-                <span className="label-text">Phone</span>
-              </label>
-              <input
-                {...register("phone", { required: true })}
-                type="number"
-                placeholder="Type here"
+                value={user.displayName}
                 className="input w-full"
               />
             </div>
@@ -52,6 +58,18 @@ const Payment = () => {
               <input
                 {...register("email", { required: true })}
                 type="email"
+                placeholder="Type here"
+                value={user.email}
+                className="input w-full"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="label-text">Phone</span>
+              </label>
+              <input
+                {...register("phone", { required: true })}
+                type="number"
                 placeholder="Type here"
                 className="input w-full"
               />
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -2,22 +2,27 @@ import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
-    
-    const location = useLocation()
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
 
-    if(loading){
-        return <div><h1 className="text-center my-20 text-3xl">Loading...</h1></div>
-    }
+  const location = useLocation();
 
-    if(user){
-        return children;
-    }
-    else{
-        return <Navigate to="/login" state={{ from: location }}></Navigate>
-    }
-    
+  if (loading) {
+    return (
+      <div>
+        <span className="loading loading-ball loading-xs"></span>
+        <span className="loading loading-ball loading-sm"></span>
+        <span className="loading loading-ball loading-md"></span>
+        <span className="loading loading-ball loading-lg"></span>
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  } else {
+    return <Navigate to="/login" state={{ from: location }}></Navigate>;
+  }
 };
 
 export default PrivateRoute;
