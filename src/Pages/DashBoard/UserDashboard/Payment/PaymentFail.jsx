75a822d9a52a194731a01a7f50e06da8701fa057
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-22 00:50:12 +0600

    add image in status page

--- src/Pages/DashBoard/UserDashboard/Payment/PaymentFail.jsx
+++ src/Pages/DashBoard/UserDashboard/Payment/PaymentFail.jsx
@@ -1,7 +1,8 @@
 const PaymentFail = () => {
   return (
-    <div className="py-36 text-center">
-      <h1 className="text-5xl text-red-500 ">Your Payment Failed!!!</h1>
+    <div className="py-36 mx-auto container">
+        <img src="https://cdn-icons-png.flaticon.com/512/6659/6659895.png" alt="" />
+      <h1 className="text-5xl mt-10 text-red-500 ">Your Payment Failed!!!</h1>
       
     </div>
   );
--- src/Pages/DashBoard/UserDashboard/Payment/PaymentSuccess.jsx
+++ src/Pages/DashBoard/UserDashboard/Payment/PaymentSuccess.jsx
@@ -1,11 +1,13 @@
-
 const PaymentSuccess = () => {
-    return (
-        <div className="py-36 flex justify-center items-start">
-           <h1 className="text-5xl text-green-600 ">Your Payment Successful!!!</h1> 
-           
-        </div>
-    );
+  return (
+    <div className="py-36 container mx-auto">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/2003/2003258.png"
+        alt=""
+      />
+      <h1 className="text-5xl mt-10 text-green-600 ">Your Payment Successful!!!</h1>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
