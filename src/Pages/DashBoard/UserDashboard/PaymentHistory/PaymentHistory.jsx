1a6caf516f3b1c697288d44cd34084691b52de17
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-25 00:31:10 +0600

    make payment history show in dashboard

--- src/Pages/DashBoard/UserDashboard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/DashBoard/UserDashboard/PaymentHistory/PaymentHistory.jsx
@@ -2,7 +2,7 @@ import useHistory from "../../../../Hook/useHistory";
 import SectionTitle from "../../../../components/SectionTitle/SectionTitle";
 
 const PaymentHistory = () => {
-  const [history] = useHistory()
+  const [history] = useHistory();
   console.log(history);
   return (
     <div className="p-10">
@@ -19,19 +19,27 @@ const PaymentHistory = () => {
                 <th>Food Image</th>
                 <th>Item Name</th>
                 <th>Price</th>
-                <th>Payment</th>
-                <th>Remove</th>
+                <th>Transaction ID</th>
+                <th>Status</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>ajskdhjk</td>
-                <td>ajskdhjk</td>
-                <td>ajskdhjk</td>
-                <td>ajskdhjk</td>
-                <td>ajskdhjk</td>
-                <td>ajskdhjk</td>
-              </tr>
+              {history.map((payment, index) => (
+                <tr key={payment._id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="avatar">
+                      <div className="w-24 rounded">
+                        <img src={payment?.item?.image} />
+                      </div>
+                    </div>
+                  </td>
+                  <td>{payment?.item?.name}</td>
+                  <td>${payment?.item?.price}</td>
+                  <td>TranID: {payment?.tranId}</td>
+                  <td className="bg-green-600 text-center text-xl">Paid</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
