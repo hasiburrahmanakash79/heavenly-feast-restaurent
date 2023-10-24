bd2ecc84183564cab710c131e99b829dcb8a7fad
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-25 00:17:26 +0600

    make payment history

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -8,7 +8,7 @@ const useAxiosSecure = () => {
   const navigate = useNavigate();
 
   const axiosSecure = axios.create({
-    // baseURL: 'https://heavenly-feast-server.vercel.app',
+    // baseURL: 'http://localhost:5000',
     baseURL: 'http://localhost:5000',
   });
 
--- src/Pages/Home/Reviews/Reviews.jsx
+++ src/Pages/Home/Reviews/Reviews.jsx
@@ -11,7 +11,7 @@ import "@smastrom/react-rating/style.css";
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    fetch("https://heavenly-feast-server.vercel.app/review")
+    fetch("http://localhost:5000/review")
       .then((res) => res.json())
       .then((data) => {
         setReviews(data);
