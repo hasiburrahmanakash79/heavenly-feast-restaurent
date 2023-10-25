3c62bfca9286c3ece8f52f42f786f9b8c9c195ca
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-25 12:05:08 +0600

    change link

--- src/Pages/Login/SignUp/SignUp.jsx
+++ src/Pages/Login/SignUp/SignUp.jsx
@@ -35,7 +35,7 @@ const SignUp = () => {
         updateUserInfo(name, photo)
           .then(() => {
             const userInfo = { name: name, email: email, role: "user" };
-            fetch("http://localhost:5000/users", {
+            fetch("https://heavenly-feast-server.vercel.app/users", {
               method: "POST",
               headers: {
                 "content-type": "application/json",
--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -20,7 +20,7 @@ const SocialLogin = () => {
           name: signInUser.displayName,
           email: signInUser.email,
         };
-        fetch("http://localhost:5000/users", {
+        fetch("https://heavenly-feast-server.vercel.app/users", {
           method: "POST",
           headers: {
             "content-type": "application/json",
