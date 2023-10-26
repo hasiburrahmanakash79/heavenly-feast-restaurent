31520c78d093f7de834fb10cf1c7fb87b9981b94
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-10-26 12:21:04 +0600

    start admin home page design

--- src/Pages/DashBoard/AdminDashboard/AdminHome.jsx
+++ src/Pages/DashBoard/AdminDashboard/AdminHome.jsx
@@ -1,11 +1,36 @@
-
+import { useContext } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const AdminHome = () => {
-    return (
-        <div>
-            
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-semibold mb-10">
+        Hi, Welcome Back! {user?.displayName}{" "}
+      </h1>
+      <div className="grid grid-cols-2">
+        <div className="bg-violet-300 flex flex-col justify-center items-center py-28">
+          <div className="avatar">
+            <div className="w-40 rounded-full">
+              {user?.photoURL ? (
+                <img src={user?.photoURL} />
+              ) : (
+                <img src="https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" />
+              )}
+            </div>
+          </div>
+          <h1 className="text-3xl mt-7">{user?.displayName} </h1>
         </div>
-    );
+        <div className="bg-yellow-200 p-28">
+          <h1 className="text-4xl font-bold mb-5">Your Activities</h1>
+          <p className="text-2xl">Orders: 6</p>
+          <p className="text-2xl">Reviews: 2</p>
+          <p className="text-2xl">Bookings: 1</p>
+          <p className="text-2xl">Bookings: 1</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
