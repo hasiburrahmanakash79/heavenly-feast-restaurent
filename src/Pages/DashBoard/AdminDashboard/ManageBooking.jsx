b1f023c42d252ce9dadaae9716f34700452e3b47
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-11-02 11:27:20 +0600

    make table

--- src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
+++ src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
@@ -1,10 +1,36 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const ManageBooking = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="p-5">
+      <SectionTitle
+        title={"manage Booking"}
+      ></SectionTitle>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>index + 1</th>
+              <td>user.name</td>
+              <td>user.email</td>
+              <td>asdsd</td>
+              <td>asd</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageBooking;
