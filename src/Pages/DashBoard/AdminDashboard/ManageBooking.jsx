b1702aa99ee0fb49476e8d9a086be07ad56c4a96
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-11-02 12:43:35 +0600

    map all booking

--- src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
+++ src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const ManageBooking = () => {
+  const [booking, setBooking] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/booking");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const result = await response.json();
+        setBooking(result);
+      } catch (error) {
+        setBooking(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(booking);
+
   return (
     <div className="p-5">
-      <SectionTitle
-        title={"manage Booking"}
-      ></SectionTitle>
+      <SectionTitle title={"manage Booking"}></SectionTitle>
       <div className="overflow-x-auto">
         <table className="table w-full">
           {/* head */}
@@ -13,19 +33,25 @@ const ManageBooking = () => {
             <tr>
               <th>No.</th>
               <th>Name</th>
+              <th>Phone</th>
               <th>Email</th>
-              <th>Role</th>
-              <th>Action</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Person</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>index + 1</th>
-              <td>user.name</td>
-              <td>user.email</td>
-              <td>asdsd</td>
-              <td>asd</td>
-            </tr>
+            {booking.map((bookTable, index) => (
+              <tr key={bookTable._id}>
+                <th>{index + 1}</th>
+                <td>{bookTable?.name}</td>
+                <td>{bookTable?.phone}</td>
+                <td>{bookTable?.email}</td>
+                <td>{bookTable?.date}</td>
+                <td>{bookTable?.time}</td>
+                <td>{bookTable?.person} person</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
