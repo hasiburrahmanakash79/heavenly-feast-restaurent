42cd00fd14b0f90e977c09934b9766ebf70adf49
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-11-05 23:46:32 +0600

    make a hook

--- src/Hook/useBooking.jsx
+++ src/Hook/useBooking.jsx
@@ -2,7 +2,7 @@
 const useBooking = () => {
     return (
         <div>
-            <h1>jajs</h1>
+            <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum exercitationem ducimus cumque accusantium numquam veritatis, recusandae rerum animi asperiores dicta voluptatum maiores magnam doloremque a alias debitis eos, nemo, maxime ipsam. Quibusdam nisi, culpa consectetur dolor delectus animi odio possimus quis voluptatem exercitationem molestias, totam eum alias at sequi aspernatur rerum quae ipsam. Amet consequuntur inventore error dolore, suscipit natus quam, assumenda odit exercitationem hic unde. Ullam excepturi harum numquam corporis laudantium qui possimus cupiditate officiis repellendus, suscipit reprehenderit. Fugit odit placeat deleniti enim incidunt saepe pariatur laborum doloribus id fuga labore, veniam ab dolorem magni corrupti culpa animi? Ex.</h1>
         </div>
     );
 };
