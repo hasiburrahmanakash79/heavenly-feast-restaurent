de724eb72353957289c002481202c1dac1ba5135
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-11-06 00:18:59 +0600

    make hook for individual bokking items

--- src/Hook/useBooking.jsx
+++ src/Hook/useBooking.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const useBooking = () => {
-    return (
-        <div>
-            <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum exercitationem ducimus cumque accusantium numquam veritatis, recusandae rerum animi asperiores dicta voluptatum maiores magnam doloremque a alias debitis eos, nemo, maxime ipsam. Quibusdam nisi, culpa consectetur dolor delectus animi odio possimus quis voluptatem exercitationem molestias, totam eum alias at sequi aspernatur rerum quae ipsam. Amet consequuntur inventore error dolore, suscipit natus quam, assumenda odit exercitationem hic unde. Ullam excepturi harum numquam corporis laudantium qui possimus cupiditate officiis repellendus, suscipit reprehenderit. Fugit odit placeat deleniti enim incidunt saepe pariatur laborum doloribus id fuga labore, veniam ab dolorem magni corrupti culpa animi? Ex.</h1>
-        </div>
-    );
+    const {user,loading} = useContext(AuthContext)
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
+
+    const { refetch, data: booking = [] } = useQuery({
+        queryKey: ['booking', user?.email],
+        enabled: !loading,
+        queryFn: async () =>{
+            const res = await axiosSecure(`/booking?email=${user?.email}`)
+            return res.data;
+        },
+      })
+      return [booking, refetch]
 };
 
 export default useBooking;
