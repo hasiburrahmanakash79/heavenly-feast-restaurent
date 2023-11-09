0db83f7cfb76f00492ee036caceb90f84dc4cb9c
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-11-09 23:58:32 +0600

    make social logo hh

--- src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
+++ src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
@@ -5,20 +5,12 @@ const ManageBooking = () => {
   const [booking, setBooking] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://heavenly-feast-server.vercel.app/booking");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const result = await response.json();
-        setBooking(result);
-      } catch (error) {
-        setBooking(error.message);
-      }
-    };
-
-    fetchData();
+    fetch("http://localhost:5000/booking")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooking(data);
+        console.log(data);
+      });
   }, []);
 
   return (
