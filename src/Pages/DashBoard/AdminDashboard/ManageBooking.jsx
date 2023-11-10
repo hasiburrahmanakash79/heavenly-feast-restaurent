7972b33b428e7edc6d257a72d8bbddce20ab0665
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-11-11 00:15:27 +0600

    delete booking table indevidual id and deploy site

--- src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
+++ src/Pages/DashBoard/AdminDashboard/ManageBooking.jsx
@@ -26,7 +26,7 @@ const ManageBooking = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        fetch(`http://localhost:5000/booking/${id}`, {
+        fetch(`https://heavenly-feast-server.vercel.app/booking/${id}`, {
           method: "DELETE",
         })
           .then((res) => res.json())
