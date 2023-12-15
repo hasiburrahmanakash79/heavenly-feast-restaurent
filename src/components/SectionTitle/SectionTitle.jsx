273cbb28475f8b38e9513342a2353815659dac34
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2023-12-15 23:45:43 +0600

    change section title for whole site

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,16 @@
 const SectionTitle = ({ subTitle, title }) => {
   return (
     <div>
-      
+      <div className="text-center md:w-1/4 mx-auto my-7">
+        <p className="text-yellow-500 py-2">{subTitle}</p>
+        <div className="mb-8 flex justify-center items-center">
+          <span className="h-1 w-14 rounded-3xl bg-gray-700" />
+          <h2 className="px-3 text-xl lg:text-3xl font-semibold uppercase">
+            {title}
+          </h2>
+          <span className="h-1 w-14 rounded-3xl bg-gray-700" />
+        </div>
+      </div>
     </div>
   );
 };
